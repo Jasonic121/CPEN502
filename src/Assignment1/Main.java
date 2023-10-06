faf2c91c14bd5707020bb264771e9c46f13b04dd
jason <trust.jason@gmail.com>
2023-10-05 23:55:41 -0700

    Modified Main

--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -1,5 +1,9 @@
 package Assignment1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,32 +26,39 @@ public class Main {
         double targetError = 0.05;  // Adjust as needed
         double totalError;
 
-        for (int epoch = 0; epoch < maxEpochs; epoch++) {
-            totalError = 0.0;
+        // Create a PrintWriter to write to the CSV file
+        try (PrintWriter writer = new PrintWriter(new FileWriter("src/Assignment1/script/graphing_data.csv"))) {
 
-            for (int i = 0; i < inputPatterns.length; i++) {
-                double[] input = inputPatterns[i];
-                double[] target = targetOutputs[i];
+            for (int epoch = 0; epoch < maxEpochs; epoch++) {
+                totalError = 0.0;
 
-                // Forward pass
-                double output = neuralNet.outputFor(input);
+                for (int i = 0; i < inputPatterns.length; i++) {
+                    double[] input = inputPatterns[i];
+                    double[] target = targetOutputs[i];
 
-                // Backpropagation and training
-                double error = neuralNet.train(input, target[0]);
+                    // Forward pass
+                    double output = neuralNet.outputFor(input);
 
-                totalError += error;
+                    // Backpropagation and training
+                    double error = neuralNet.train(input, target[0]);
+
+                    totalError += error;
+                }
+
+                // Calculate the average error for this epoch
+                double averageError = totalError / inputPatterns.length;
+
+                // Check if the training has reached the target error
+                if (averageError < targetError) {
+                    System.out.println("Training completed in " + (epoch + 1) + " epochs.");
+                    break;
+                }
+                System.out.println("Epoch:" + epoch + "  "+"averageError:" + averageError);
+                writer.print("Epoch: "); writer.println(epoch);
+                writer.print("averageError: "); writer.println(averageError);
+
             }
 
-            // Calculate the average error for this epoch
-            double averageError = totalError / inputPatterns.length;
-
-            // Check if the training has reached the target error
-            if (averageError < targetError) {
-                System.out.println("Training completed in " + (epoch + 1) + " epochs.");
-                break;
-            }
-            System.out.println("Epoch:" + epoch + "  "+"averageError:" + averageError);
-        }
 
         // Test the trained neural network
         System.out.println("Testing trained neural network:");
@@ -56,39 +67,8 @@ public class Main {
             double output = neuralNet.outputFor(input);
             System.out.println("Input: " + input[0] + ", " + input[1] + " => Output: " + output);
         }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-//    // Method to create and display the graph
-//    private static void createEpochVsAverageErrorGraph(List<Double> epochList, List<Double> averageErrorList) {
-//        DefaultXYDataset dataset = new DefaultXYDataset();
-//        double[][] data = new double[2][epochList.size()];
-//
-//        for (int i = 0; i < epochList.size(); i++) {
-//            data[0][i] = epochList.get(i);
-//            data[1][i] = averageErrorList.get(i);
-//        }
-//
-//        dataset.addSeries("Epoch vs. Average Error", data);
-//
-//        JFreeChart chart = ChartFactory.createXYLineChart(
-//                "Epoch vs. Average Error",
-//                "Epoch",
-//                "Average Error",
-//                dataset,
-//                PlotOrientation.VERTICAL,
-//                true,
-//                true,
-//                false
-//        );
-//
-//        XYPlot plot = chart.getXYPlot();
-//        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, false);
-//        plot.setRenderer(renderer);
-//
-//        JFrame frame = new JFrame("Epoch vs. Average Error");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.getContentPane().add(new ChartPanel(chart));
-//        frame.pack();
-//        frame.setVisible(true);
-//    }
 }
